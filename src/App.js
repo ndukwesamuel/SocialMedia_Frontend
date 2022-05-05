a572c539ad5b3ea49c59eac58b158fdfd2dc4201
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-05-05 11:51:21 +0100

    new work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Topbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<div> About</div>} />
+          <Route path="/service" element={<div> service </div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Person from "@mui/icons-material/Person";
+
+function Home() {
+  return (
+    <div>
+      <h1> Home</h1>
+      <Person />
+    </div>
+  );
+}
+
+export default Home;
